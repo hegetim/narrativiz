5827b35f7fa302b0fbf712731fec189afb38ffea
Tim Hegemann <tim.hegemann@uni-wuerzburg.de>
2025-01-13 11:44:04 +0100

    backup

--- src/components/StorylineFromFile.tsx
+++ src/components/StorylineFromFile.tsx
@@ -3,7 +3,9 @@
  */
 
 import React, { useCallback, useRef, useState } from "react";
-import { cls } from "../model/Utils";
+import { cls, matchByKind, unimplemented } from "../model/Utils";
+import { master2storyline, sgbFile } from "../io/sgb";
+import { formatError } from "parsimmon";
 
 type Props = {};
 type State = { kind: 'calm' | 'active' } | { kind: 'error', msg: string };
@@ -12,9 +14,16 @@ export const SelectFile = ({ }: Props) => {
   const hiddenInput = useRef<HTMLInputElement>(null);
   const [state, setState] = useState<State>({ kind: 'calm' });
 
-  const handleFiles = useCallback((files: FileList | null) => {
-
-  }, []);
+  const handleFiles = async (files: FileList | null) => {
+    const checked = await checkFiles(files);
+    matchByKind(checked, {
+      error: err => setState(err),
+      pass: pass => matchByKind(parseMaster(pass.str), {
+        error: err => setState(err),
+        pass: pass => unimplemented(),
+      })
+    });
+  };
 
   const handleDrop = useCallback((ev: React.DragEvent<HTMLDivElement>) => {
     ev.preventDefault();
@@ -40,7 +49,17 @@ export const SelectFile = ({ }: Props) => {
 }
 
 const checkFiles = async (files: FileList | null) => {
-  if (!files?.length) { return { type: 'error', msg: "something went wrong" }; }
-  else if (files.length !== 1) { return { type: 'error', msg: "please upload only one file at a time" }; }
-  else { return files[0]?.text().then(str => ({ kind: 'pass', str })); }
+  if (!files?.length) { return { kind: 'error' as const, msg: "something went wrong" }; }
+  else if (files.length !== 1) { return { kind: 'error' as const, msg: "please upload only one file at a time" }; }
+  else { return files[0]!.text().then(str => ({ kind: 'pass' as const, str })); }
+}
+
+const parseMaster = (raw: string) => {
+  const parsed = sgbFile('loose', 'master').parse(raw);
+  if (!parsed.status) {
+    return { kind: 'error' as const, msg: `Could not parse masster file: ${formatError(raw, parsed)}` };
+  }
+  const story = master2storyline(parsed.value);
+  console.log(story);
+  return { kind: 'pass' as const, story };
 }
